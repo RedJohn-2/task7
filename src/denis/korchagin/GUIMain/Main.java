c0afe78b630009dcb80cca7f7982936958ac801e
RedJohn-2 <LakadMatataag@mail.ru>
2020-12-06 20:07:04 +0300

    hello

--- src/denis/korchagin/GUIMain/Main.java
+++ src/denis/korchagin/GUIMain/Main.java
@@ -3,6 +3,5 @@ package denis.korchagin.GUIMain;
 public class Main {
     public static void main(String[] args) {
         FrameMain task7 = new FrameMain();
-        task7.setVisible(true);
     }
 }
